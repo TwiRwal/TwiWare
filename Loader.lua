f4f54342ca5d3209f0ae8dfb72543fddf79f0d20
TwiRwal <imtwirwal@gmail.com>
2024-09-08 09:39:58 +0530

    New

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,44 @@
+-- Open Sourced Loader (For Skids)
 
+repeat task.wait() until game:IsLoaded();
+--// Notify
+game.StarterGui:SetCore("SendNotification", {
+    Title = "TwiWare"; 
+    Text = "Injected Successfully"; 
+    Icon = ""; 
+    Duration = 5;
+    Callback = bindableFunction;
+    Button1 = "Cool";  
+    Button2 = "";
+})
+game.StarterGui:SetCore("SendNotification", {
+    Title = "TwiWare"; 
+    Text = "We Are Working On With BladeBall So Don't Worry"; 
+    Icon = ""; 
+    Duration = 5;
+    Callback = bindableFunction;
+})
+
+--// Gui
+local Library = loadstring(game:HttpGet("https://bitbucket.org/cat__/uis/raw/Loady/Module"), "Twi:3")()
+local Window = Library:Window("TwiWare - v2.0.4", function(Script)
+        loadstring(game:HttpGet(Script))()
+end)
+
+Window:Game({
+   Place = 13772394625,
+   Status = "Little Patched",
+   Script = "https://raw.githubusercontent.com/TwiWare/TwiWareExploits/main/ObfGames/Bladeball-fixed",
+   UpdateDate = Window:GetDate("Local", "3/13/24"),
+   Creator = "TwiRwal And SumitScripts"
+})
+
+Window:Game({
+   Place = 10449761463,
+   Status = "Working + Updated",
+   Script = "",
+   UpdateDate = Window:GetDate("Local", "3/13/24"),
+   Creator = "TwiRwal And SumitScripts"
+})
+
+Window:Initiate()
