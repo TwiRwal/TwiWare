a2485f033b8ed19de68e98aa58260b2c7c16595e
TwiRwal <imtwirwal@gmail.com>
2024-09-10 16:05:02 +0530

    Deleted Loadstring.lua Becuz I Thought It's Not Gonna On Auto Executer

--- Loadstring.lua
+++
@@ -1,7 +0,0 @@
--- Don't Use This Loadstring This is How You Gonna Get Updates Man.
--- In Development
-getgenv().TwiWare = "https://raw.githubusercontent.com/TwiRwal/TwiWare/main/Loader.lua";
-
-    pcall(function()
-        loadstring(game:HttpGet(getgenv().TwiWare))();
-    end)
