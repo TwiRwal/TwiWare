b9349fc151f24d8524ac2cacaae4c77049481511
TwiRwal <imtwirwal@gmail.com>
2024-09-10 19:11:22 +0530

    Update! TESTING

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,10 @@
 -- Open Sourced Loader
--- AutoExecuter
+-- Auto Loader
  not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
+-- AutoExecuter
 if _G.AutoExecuter == true then
 local AutoExeScript = queueonteleport or queue_on_teleport
 if AutoExeScript then
@@ -23,6 +24,7 @@ Text = "Failed To Auto Execute";
 Icon = ""; 
 Duration = 5; 
 })
+ 
 --// Notify
 game.StarterGui:SetCore("SendNotification", {
     Title = "TwiWare"; 
@@ -49,7 +51,7 @@ end)
 
 Window:Game({
    Place = 13772394625,
-   Status = "Little Patched",
+   Status = "Patched (Working On It)",
    Script = "https://raw.githubusercontent.com/TwiWare/TwiWareExploits/main/ObfGames/Bladeball-fixed",
    UpdateDate = Window:GetDate("Local", "3/13/24"),
    Creator = "TwiRwal And SumitScripts"
